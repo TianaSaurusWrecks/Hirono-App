3cacaa2e5c79d7bb170ffdab11319d678943941c
TianaSaurusWrecks <tianavioletwilson@gmail.com>
2026-05-06 11:02:52 +1000

    Update thanks.js
    fixed typo

--- src/pages/thanks.js
+++ src/pages/thanks.js
@@ -1,17 +1,19 @@
 import Link from "next/link";
 
 export default function Thanks() {
-    return (
-        <div className="page">
-            <h1 className="collection">Thanks for your Message!</h1>
-            <p className="about-text">I really appreciate you reaching out to me and I will get back to you soon as I can.</p>
+  return (
+    <div className="page">
+      <h1 className="collection">Thanks for your Message!</h1>
+      <p className="about-text">
+        I really appreciate you reaching out to me and I will get back to you as
+        soon as I can.
+      </p>
 
-            <div className="home-btn-wrapper">
-            <Link href="/" className="home-button">
-            Back to Home
-            </Link>
-            </div>
-
-        </div>
-    );
+      <div className="home-btn-wrapper">
+        <Link href="/" className="home-button">
+          Back to Home
+        </Link>
+      </div>
+    </div>
+  );
 }
